16c9d6b4033dc4cf02bfef010380c8b0cc42a801
rohitbhatt1 <rohitsharma80449@gmail.com>
2021-08-14 13:05:49 +0530

    push assignment

---
+++ Documents/Web Development/HTML&CSSFundamentals/Basic-Positioning/positioningA-1/index.js
@@ -0,0 +1,162 @@
+/*<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Asssignment 1 | positioning</title>
+        <link rel="stylesheet" href="style.css">
+        <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="cat">
+
+    </head>
+    <body>
+        <header>
+            <h1>Extracts from "Our Cats, By Harrison Weir"</h1>
+        </header>
+        <main>
+            <section>
+                <div class="container">
+                    <aside class="col col1">
+                        <p>Introductory</p>
+                        <p>The First Cat Show</p>
+                        <p>Habits</p>
+                        <p>Trained Cats</p>
+                        <p>Usefulness of Cats</p>
+                    </aside>
+                    <article class="col col2">
+                        <h2>Usefulness of cats</h2>
+                        <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="cat">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse autem suscipit libero repellat nihil voluptatem dolores sint soluta nam minus, reprehenderit ipsa at, obcaecati consequuntur corrupti cum excepturi consectetur. Et iure repellendus ab dicta eius sint fuga quae, officia nemo dolorem quidem autem expedita nulla deleniti perspiciatis asperiores tempore vitae!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, atque illum tenetur harum amet distinctio recusandae vitae neque adipisci facilis ab voluptas non, ad voluptatum.</p>
+                        <p class="quote">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, autem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, autem.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis odio officiis ad consequuntur saepe velit, laudantium qui nobis earum quia, quidem eaque. Assumenda, optio odio?</p>
+   
+                    </article>
+                    <aside class="col col3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipis elit. Ipsum sint eos minus inventore tempore at dolor consectetur adipisicing elit. Cum sed harum </p>
+                    </aside>
+                </div>
+            </section>
+        
+        </main>
+    </body>
+</html>
+            
+            
+            
+            
+            //
+            html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+/* costum style */
+*{
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+}
+body{
+    font-size: 16px;
+    line-height: 1.5;
+    padding:32px;
+    background-color: antiquewhite;
+}
+header{
+    text-align: center;
+}
+.container{
+    width: 1200px;
+    margin: 0 auto;
+    padding:   32px;
+    background-color: white;
+}
+.container::after ,.container::before{
+    content: '';
+    display: table;
+}
+.container::after{
+    clear: both;
+}
+h1{
+    font-size: 60px;
+    font-weight: 500;
+    word-spacing: 10px;
+    text-align: center;
+    padding:24px 0 ;
+}
+img{
+    width: 100%;
+}
+.col{
+    float: left;
+    vertical-align: top;
+}
+.col2{
+    width: 50%;
+}
+aside{
+    width: 25%;
+}
+h2{
+    font-size: 36px;
+    font-weight: 700;
+    margin-bottom: 16px;
+}
+.col2 p{
+    font-size: 16px;
+    margin-top: 16px;
+}
+.quote{
+    padding: 0 32px;
+}
+.col1 p{
+    font-size: 20px;
+}
+.col3{
+    font-weight: 600px;
+    padding-left: 32px;
+    font-size: 20px;
+    font-weight: 600;
+}*/
+
+
+            
